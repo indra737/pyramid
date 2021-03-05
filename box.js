77183d7b35b1ec6d57b2bd67b491893e7186ffdf
indra737 <76808535+indra737@users.noreply.github.com>
2021-03-05 14:45:44 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,39 @@
+class Box{
+    constructor(x, y,w,h,r,g,b) {
+        var options = {
+            'restitution':0.9,
+            'friction':1,
+            'density': 0.5
+            
+        }
+
+        this.width =w;
+        this.height =h;
+        this.body = Bodies.rectangle(x, y, this.width,this.height, options);
+      this.r=r
+      this.g=g
+      this.b=b
+        this.visibility=255
+        World.add(world, this.body);
+      }
+     
+      show(){
+        var pos =this.body.position;
+        var angle= this.body.angle;
+      
+        
+        push()
+       
+        translate(pos.x,pos.y)
+        rotate(angle)
+       
+        rectMode(CENTER);
+        fill(this.r,this.g,this.b);
+        stroke(0)
+        rect(0,0,this.width,this.height);
+        pop()
+      
+
+      
+    }
+}
